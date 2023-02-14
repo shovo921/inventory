d8567f10a1eab95332463ffd5b532d897b7f93ef
shovo922 <shuvo@kaicomsol.com>
2023-02-14 17:43:31 +0600

    sidebar updated

--- resources/views/backend/admin/partials/sidebar.blade.php
+++ resources/views/backend/admin/partials/sidebar.blade.php
@@ -22,9 +22,10 @@
         </li>
 
         <li class="nav-item">
-            <a class="nav-link collapsed " data-bs-target="#components-nav-c" data-bs-toggle="collapse"  href="#"> <i class="bi bi-menu-button-wide"></i><span>Conatct</span><i class="bi bi-chevron-down ms-auto"></i> </a>
+            <a class="nav-link collapsed " data-bs-target="#components-nav-c" data-bs-toggle="collapse"  href="#"> <i class="bi bi-menu-button-wide"></i><span>Pakage</span><i class="bi bi-chevron-down ms-auto"></i> </a>
             <ul id="components-nav-c" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-                <li> <a href="{{route('conatct.list')}}"> <i class="bi bi-circle"></i><span>Conatct list</span> </a></li>
+                <li> <a href="{{route('conatct.list')}}"> <i class="bi bi-circle"></i><span>Pakage list</span> </a></li>
+                <li> <a href="{{route('conatct.list')}}"> <i class="bi bi-circle"></i><span>Pakage create</span> </a></li>
 
             </ul>
         </li>
@@ -50,7 +51,7 @@
             </ul>
         </li>
         <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#"> <i class="bi bi-gem"></i><span>Icons</span><i class="bi bi-chevron-down ms-auto"></i> </a>
+            <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#"> <i class="bi bi-gem"></i><span>User</span><i class="bi bi-chevron-down ms-auto"></i> </a>
             <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                 <li> <a href="icons-bootstrap.html"> <i class="bi bi-circle"></i><span>Bootstrap Icons</span> </a></li>
                 <li> <a href="icons-remix.html"> <i class="bi bi-circle"></i><span>Remix Icons</span> </a></li>
@@ -58,12 +59,12 @@
             </ul>
         </li>
         <li class="nav-heading">Pages</li>
+        <li class="nav-item"> <a class="nav-link collapsed" href="{{route('conatct.list')}}"> <i class="bi bi-envelope"></i> <span>Contact</span> </a></li>
         <li class="nav-item"> <a class="nav-link collapsed" href="users-profile.html"> <i class="bi bi-person"></i> <span>Profile</span> </a></li>
-        <li class="nav-item"> <a class="nav-link collapsed" href="pages-faq.html"> <i class="bi bi-question-circle"></i> <span>F.A.Q</span> </a></li>
-        <li class="nav-item"> <a class="nav-link collapsed" href="pages-contact.html"> <i class="bi bi-envelope"></i> <span>Contact</span> </a></li>
-        <li class="nav-item"> <a class="nav-link collapsed" href="pages-register.html"> <i class="bi bi-card-list"></i> <span>Register</span> </a></li>
-        <li class="nav-item"> <a class="nav-link collapsed" href="pages-login.html"> <i class="bi bi-box-arrow-in-right"></i> <span>Login</span> </a></li>
-        <li class="nav-item"> <a class="nav-link collapsed" href="pages-error-404.html"> <i class="bi bi-dash-circle"></i> <span>Error 404</span> </a></li>
-        <li class="nav-item"> <a class="nav-link collapsed" href="pages-blank.html"> <i class="bi bi-file-earmark"></i> <span>Blank</span> </a></li>
+        <li class="nav-item"> <a class="nav-link collapsed" href="{{ route('admin.logout') }}"> <i class="bi bi-box-arrow-in-right"></i> <span>Logout</span> </a></li>
+{{--        <li class="nav-item"> <a class="nav-link collapsed" href="pages-faq.html"> <i class="bi bi-question-circle"></i> <span>F.A.Q</span> </a></li>--}}
+{{--        <li class="nav-item"> <a class="nav-link collapsed" href="pages-register.html"> <i class="bi bi-card-list"></i> <span>Register</span> </a></li>--}}
+{{--        <li class="nav-item"> <a class="nav-link collapsed" href="pages-error-404.html"> <i class="bi bi-dash-circle"></i> <span>Error 404</span> </a></li>--}}
+{{--        <li class="nav-item"> <a class="nav-link collapsed" href="pages-blank.html"> <i class="bi bi-file-earmark"></i> <span>Blank</span> </a></li>--}}
     </ul>
 </aside>
